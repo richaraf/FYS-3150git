192aa4957bf14a2778606d59ee671e571b4d9615
Richard Fauli <richaraf@student.matnat.uio.no>
2016-09-07 15:52:23 +0200

    Made it so it makes more plots from different .txt files.

--- Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
+++ Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
@@ -1,6 +1,6 @@
 from scitools.std import *
 
-with open('1b.txt', 'r') as ins:
+with open('1bn10.txt', 'r') as ins:
 	v = []
 	for line in ins:
 		v.append(float(line))
@@ -13,4 +13,73 @@ for i in range(n):
 	x[i] = i*h
 
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
-plot(x,v,x,anasol,xlabel='x',ylabel='v(x)',legend=('numerical','analytical'))
+figure(1)
+subplot(2,1,1)
+plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=10')
+diff = abs(anasol-v)
+reldiff = diff/v
+subplot(2,1,2)
+plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+
+with open('1bn100.txt', 'r') as ins:
+	v = []
+	for line in ins:
+		v.append(float(line))
+
+v = array(v)
+n = len(v)
+h = 1.0/(n+1);
+x = zeros(n)
+for i in range(n):
+	x[i] = i*h
+
+anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
+figure(2)
+subplot(2,1,1)
+plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=100')
+diff = abs(anasol-v)
+reldiff = diff/v
+subplot(2,1,2)
+plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+
+with open('1bn1000.txt', 'r') as ins:
+	v = []
+	for line in ins:
+		v.append(float(line))
+
+v = array(v)
+n = len(v)
+h = 1.0/(n+1);
+x = zeros(n)
+for i in range(n):
+	x[i] = i*h
+
+anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
+figure(3)
+subplot(2,1,1)
+plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=1000')
+diff = abs(anasol-v)
+reldiff = diff/v
+subplot(2,1,2)
+plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+
+with open('1bn10000.txt', 'r') as ins:
+	v = []
+	for line in ins:
+		v.append(float(line))
+
+v = array(v)
+n = len(v)
+h = 1.0/(n+1);
+x = zeros(n)
+for i in range(n):
+	x[i] = i*h
+
+anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
+figure(4)
+subplot(2,1,1)
+plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=10000')
+diff = abs(anasol-v)
+reldiff = diff/v
+subplot(2,1,2)
+plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
