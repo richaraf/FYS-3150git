7922ac5ff5694ff11e5e122f6c26925dfbc2a383
Richard Fauli <richaraf@student.matnat.uio.no>
2016-09-07 12:28:16 +0200

    Commit all the example files.

--- testingqt/main.cpp
+++ testingqt/main.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+void doSomething(int *a, int *b) {
+    // cout << " Address of a inside: " << (long)&a << endl;
+    *a = 2*(*a);
+    *b = 2*(*b);
+}
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    // Two operators:
+    // & address-of operator
+    // * dereference operator
+
+    int a = 10;
+    int b = 20;
+    // cout << " Address of a outside: " << (long)&a << endl;
+    doSomething(&a,&b);
+    cout << "a: " << a << endl;
+    cout << "b: " << b << endl;
+
+    //int *array = new int[10000000];
+    // int array2[10000000];
+
+    //cout << 3 << endl;
     return 0;
 }
--- warmup/warmupproject/learningforloops.cpp
+++ warmup/warmupproject/learningforloops.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    cout << "Hello World!" << endl;
+    int a;
+    cout << " This is me introducing myself to for loops: " << endl;
+
+    for (a= 0; a <= 10; a = a + 1)
+        cout << a << "\t" << a+10 << endl;
     return 0;
 }
