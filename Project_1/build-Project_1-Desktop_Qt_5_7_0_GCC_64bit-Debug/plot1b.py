e8303cbf12999a487efc52c027db2d942f781c00
Richard Fauli <richaraf@student.matnat.uio.no>
2016-09-07 14:28:08 +0200

    Program now plots num and ana sol.

--- Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
+++ Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
@@ -1,6 +1,16 @@
 from scitools.std import *
+
 with open('1b.txt', 'r') as ins:
 	v = []
 	for line in ins:
 		v.append(float(line))
+
 v = array(v)
+n = len(v)
+h = 1.0/(n+1);
+x = zeros(n)
+for i in range(n):
+	x[i] = i*h
+
+anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
+plot(x,v,x,anasol,xlabel='x',ylabel='v(x)',legend=('numerical','analytical'))
