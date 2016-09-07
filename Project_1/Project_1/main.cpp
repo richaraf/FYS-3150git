ca2250689a21365d6c99477d8f968782bce533b3
Richard Fauli <richaraf@student.matnat.uio.no>
2016-09-07 12:27:17 +0200

    Deleted gaussianelim.cpp and moved its contents to main.cpp, and defined som vectors.

--- Project_1/Project_1/main.cpp
+++ Project_1/Project_1/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    int n = 10; // dimensions of n x n matrix
+    double a[n], b[n], c[n], f[n], d[n], k[n], v[n];
     return 0;
 }
