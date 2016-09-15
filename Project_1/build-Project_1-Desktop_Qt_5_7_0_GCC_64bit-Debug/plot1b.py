b8f56ffb9ccb567b17a7f8912b43f935fc1b5365
Richard Fauli <Richard Fauli>
2016-09-15 12:07:33 +0200

    Started writing method 1

--- Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
+++ Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
@@ -4,82 +4,58 @@ with open('1bn10.txt', 'r') as ins:
 	v = []
 	for line in ins:
 		v.append(float(line))
-
 v = array(v)
 n = len(v)
 h = 1.0/(n+1);
 x = zeros(n)
-for i in range(n):
-	x[i] = i*h
+for i in range(1,n+1):
+	x[i-1] = i*h
 
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(1)
-subplot(2,1,1)
+#subplot(2,1,1)
 plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=10')
-diff = abs(anasol-v)
-reldiff = diff/v
-subplot(2,1,2)
-plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+#diff = abs(anasol-v)
+#reldiff = diff/v
+#subplot(2,1,2)
+#plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
 
 with open('1bn100.txt', 'r') as ins:
 	v = []
 	for line in ins:
 		v.append(float(line))
-
 v = array(v)
 n = len(v)
 h = 1.0/(n+1);
 x = zeros(n)
-for i in range(0,n):
-	x[i] = i*h
+for i in range(1,n+1):
+	x[i-1] = i*h
 
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(2)
-subplot(2,1,1)
+#subplot(2,1,1)
 plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=100')
-diff = abs(anasol-v)
-reldiff = diff/v
-subplot(2,1,2)
-plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+#diff = abs(anasol-v)
+#reldiff = diff/v
+#subplot(2,1,2)
+#plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
 
 with open('1bn1000.txt', 'r') as ins:
 	v = []
 	for line in ins:
 		v.append(float(line))
-
 v = array(v)
 n = len(v)
 h = 1.0/(n+1);
 x = zeros(n)
-for i in range(n):
-	x[i] = i*h
+for i in range(1,n+1):
+	x[i-1] = i*h
 
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(3)
-subplot(2,1,1)
+#subplot(2,1,1)
 plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=1000')
-diff = abs(anasol-v)
-reldiff = diff/v
-subplot(2,1,2)
-plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
-
-with open('1bn10000.txt', 'r') as ins:
-	v = []
-	for line in ins:
-		v.append(float(line))
-
-v = array(v)
-n = len(v)
-h = 1.0/(n+1);
-x = zeros(n)
-for i in range(n):
-	x[i] = i*h
-
-anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
-figure(4)
-subplot(2,1,1)
-plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),title='n=10000')
-diff = abs(anasol-v)
-reldiff = diff/v
-subplot(2,1,2)
-plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
+#diff = abs(anasol-v)
+#reldiff = diff/v
+#subplot(2,1,2)
+#plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
