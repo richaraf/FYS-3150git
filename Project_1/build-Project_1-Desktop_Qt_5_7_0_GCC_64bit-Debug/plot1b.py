7c261f9e9663af3f22ab8cf4b86fd6fa9d93fc9a
Richard Fauli <Richard Fauli>
2016-09-15 16:14:05 +0200

    Added start and end-point to the plots

--- Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
+++ Project_1/build-Project_1-Desktop_Qt_5_7_0_GCC_64bit-Debug/plot1b.py
@@ -1,16 +1,16 @@
 from scitools.std import *
 
 with open('1bn10.txt', 'r') as ins:
-	v = []
+	v = [0.0]
 	for line in ins:
 		v.append(float(line))
+v.append(0.0)
 v = array(v)
-n = len(v)
+n = len(v)-2
 h = 1.0/(n+1);
-x = zeros(n)
-for i in range(1,n+1):
-	x[i-1] = i*h
-
+x = zeros(n+2)
+for i in range(0,n+2):
+	x[i] = i*h
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(1)
 #subplot(2,1,1)
@@ -21,17 +21,16 @@ plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),tit
 #plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
 
 with open('1bn100.txt', 'r') as ins:
-	v = []
+	v = [0.0]
 	for line in ins:
 		v.append(float(line))
-
+v.append(0.0)
 v = array(v)
-n = len(v)
+n = len(v)-2
 h = 1.0/(n+1);
-x = zeros(n)
-for i in range(1,n+1):
-	x[i-1] = i*h
-
+x = zeros(n+2)
+for i in range(0,n+2):
+	x[i] = i*h
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(2)
 #subplot(2,1,1)
@@ -42,16 +41,16 @@ plot(x,v,x,anasol,xlabel='x',ylabel='u(x)',legend=('numerical','analytical'),tit
 #plot(x,log10(reldiff),xlabel='x',ylabel='log10(relative error)')
 
 with open('1bn1000.txt', 'r') as ins:
-	v = []
+	v = [0.0]
 	for line in ins:
 		v.append(float(line))
+v.append(0.0)
 v = array(v)
-n = len(v)
+n = len(v)-2
 h = 1.0/(n+1);
-x = zeros(n)
-for i in range(1,n+1):
-	x[i-1] = i*h
-
+x = zeros(n+2)
+for i in range(0,n+2):
+	x[i] = i*h
 anasol = 1 - (1 - exp(-10))*x - exp(-10*x)
 figure(3)
 #subplot(2,1,1)
